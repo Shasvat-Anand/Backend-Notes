936bb02ad91e15c6bcbfb469df3f289cff5d4620
Shasvat-Anand <anandk84a@gmail.com>
2025-09-27 13:03:36 +0530

    add diceroller and instagram ejs

--- EJS/index.js
+++ EJS/index.js
@@ -15,6 +15,17 @@ app.get("/" , (req,res)=>{
     res.render("home")
 })
 
+app.get("/ig/:username",(req,res)=>{
+    let {username} = req.params
+    res.render("instagram.ejs",{username})
+})
+
+
+
+app.get("/rolldice" , (req ,res)=>{
+    let dicval = Math.floor(Math.random() *6)+1
+    res.render("rolldice",{num :dicval})
+})
 
 
 
